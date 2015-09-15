299fb92de0cb459e2c7176be342a1c1d71592aea
jcalixtofiguei <joao@wlan-145-94-253-053.wlan.tudelft.nl>
2015-09-15 03:25:49 +0100

    Added Portuguese Portuguese translation (pt_PT)

---
+++ translations/pt_PT.ts
@@ -0,0 +1,484 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>BookmarkItem</name>
+    <message>
+        <location filename="../src/qml/BookmarkItem.qml" line="83"/>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BookmarkItem.qml" line="89"/>
+        <source>Add to dash</source>
+        <translation>Adicionar ao painel</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BookmarkItem.qml" line="95"/>
+        <source>Delete</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BookmarkItem.qml" line="127"/>
+        <source>Edit bookmark</source>
+        <translation>Editar marcador</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BookmarkItem.qml" line="141"/>
+        <source>Title</source>
+        <translation>Título</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BookmarkItem.qml" line="149"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BookmarkItem.qml" line="158"/>
+        <source>Icon URL</source>
+        <translation>URL do ícone</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BookmarkItem.qml" line="172"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BookmarkItem.qml" line="190"/>
+        <source>Apply</source>
+        <translation>Aplicar</translation>
+    </message>
+</context>
+<context>
+    <name>BrowserOxideWebView</name>
+    <message>
+        <location filename="../src/qml/BrowserOxideWebView.qml" line="21"/>
+        <source>New tab</source>
+        <translation>Novo separador</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserOxideWebView.qml" line="21"/>
+        <source>Settings</source>
+        <translation>Opções</translation>
+    </message>
+</context>
+<context>
+    <name>BrowserPage</name>
+    <message>
+        <location filename="../src/qml/BrowserPage.qml" line="27"/>
+        <source>New window</source>
+        <translation>Nova janela</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserPage.qml" line="33"/>
+        <source>New tab</source>
+        <translation>Novo separador</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserPage.qml" line="47"/>
+        <source>History</source>
+        <translation>Histórico</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserPage.qml" line="52"/>
+        <source>Fullscreen</source>
+        <translation>Ecrã inteiro</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserPage.qml" line="62"/>
+        <location filename="../src/qml/BrowserPage.qml" line="164"/>
+        <source>Search</source>
+        <translation>Pesquisa</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserPage.qml" line="68"/>
+        <source>Bookmark</source>
+        <translation>Marcador</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserPage.qml" line="74"/>
+        <source>Add to dash</source>
+        <translation>Adicionar ao painel</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserPage.qml" line="80"/>
+        <source>View source</source>
+        <translation>Ver código-fonte</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserPage.qml" line="86"/>
+        <source>Settings</source>
+        <translation>Opções</translation>
+    </message>
+</context>
+<context>
+    <name>BrowserWebView</name>
+    <message>
+        <location filename="../src/qml/BrowserWebView.qml" line="23"/>
+        <source>New tab</source>
+        <translation>Novo separador</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserWebView.qml" line="23"/>
+        <source>Settings</source>
+        <translation>Opções</translation>
+    </message>
+</context>
+<context>
+    <name>BrowserWindow</name>
+    <message>
+        <location filename="../src/qml/BrowserWindow.qml" line="14"/>
+        <source>Loading</source>
+        <translation>A carregar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserWindow.qml" line="183"/>
+        <source>Added website &quot;%1&quot; to dash</source>
+        <extracomment>%1 is a title</extracomment>
+        <translation>Website &quot;%1&quot; adicionado ao painel</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserWindow.qml" line="432"/>
+        <source>Removed bookmark %1</source>
+        <translatorcomment>Perhaps the English version should also contain the quotes, for consistency with the other sources?</translatorcomment>
+        <translation>Marcador &quot;%1&quot; removido</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserWindow.qml" line="435"/>
+        <source>Added bookmark &quot;%1&quot;</source>
+        <translation>Marcador &quot;%1&quot; adicionado</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserWindow.qml" line="485"/>
+        <source>Reopen</source>
+        <translation>Reabrir</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserWindow.qml" line="500"/>
+        <source>This Connection Is Untrusted</source>
+        <translation>Esta conexão não é confiável</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserWindow.qml" line="502"/>
+        <source>You are about to securely connect to %1 but we can&apos;t confirm that your connection is secure because this site&apos;s identity can&apos;t be verified.</source>
+        <extracomment>%1 is an URL</extracomment>
+        <translation>Está prestes a conectar-se em segurança a &quot;%1&quot;, mas não podemos confirmar que a sua conexão seja segura já que a identidade do webiste não pode ser verificada.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserWindow.qml" line="503"/>
+        <source>Continue anyway</source>
+        <translation>Continuar mesmo assim</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BrowserWindow.qml" line="504"/>
+        <source>Leave page</source>
+        <translation>Abandonar página</translation>
+    </message>
+</context>
+<context>
+    <name>ColorChooser</name>
+    <message>
+        <location filename="../src/qml/ColorChooser.qml" line="18"/>
+        <source>Choose a color</source>
+        <translation>Escolha uma cor</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadsDrawer</name>
+    <message>
+        <location filename="../src/qml/DownloadsDrawer.qml" line="35"/>
+        <source>Downloads</source>
+        <translation>Downloads</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DownloadsDrawer.qml" line="41"/>
+        <source>Clear</source>
+        <translation>Limpar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DownloadsDrawer.qml" line="80"/>
+        <source>Awaiting confirmation</source>
+        <translation>A aguardar confirmação</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DownloadsDrawer.qml" line="82"/>
+        <source>Downloading...</source>
+        <translation>A descarregar...</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DownloadsDrawer.qml" line="84"/>
+        <source>Completed</source>
+        <translation>Concluído</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DownloadsDrawer.qml" line="86"/>
+        <source>Cancelled</source>
+        <translation>Cancelado</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DownloadsDrawer.qml" line="88"/>
+        <source>Download failed</source>
+        <translation>Download falhado</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DownloadsDrawer.qml" line="90"/>
+        <source>Unknown state!</source>
+        <translation>Estado desconhecido!</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryDrawer</name>
+    <message>
+        <location filename="../src/qml/HistoryDrawer.qml" line="30"/>
+        <source>History</source>
+        <translation>Histórico</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/HistoryDrawer.qml" line="36"/>
+        <source>Clear History</source>
+        <translation>Limpar Histórico</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/HistoryDrawer.qml" line="85"/>
+        <source>No browser history found</source>
+        <translation>Nenhum histórico encontrado</translation>
+    </message>
+</context>
+<context>
+    <name>NewTabPage</name>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="15"/>
+        <source>Nothing here, yet</source>
+        <translation>Nada aqui, por enquanto</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="27"/>
+        <source>You can add items by clicking on the menu item &quot;Add to dash&quot; on any website or by right clicking on a bookmark.</source>
+        <translation>Pode adicionar items clicando no item do menu &quot;Adicionar ao painel&quot; em qualquer website ou ao clicar com o botão direito num marcador.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="160"/>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="169"/>
+        <source>Delete</source>
+        <translation>Apagar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="207"/>
+        <source>Edit item</source>
+        <translation>Editar item</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="221"/>
+        <source>Title</source>
+        <translation>Título</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="229"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="238"/>
+        <source>Icon URL</source>
+        <translation>URL do ícone</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="247"/>
+        <source>Background color</source>
+        <translation>Cor de fundo</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="259"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NewTabPage.qml" line="274"/>
+        <source>Apply</source>
+        <translation>Aplicar</translation>
+    </message>
+</context>
+<context>
+    <name>Omnibox</name>
+    <message>
+        <location filename="../src/qml/Omnibox.qml" line="104"/>
+        <source>Search</source>
+        <translation>Pesquisa</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Omnibox.qml" line="104"/>
+        <source>Search or enter website name</source>
+        <translatorcomment>URL would be a better term for &quot;website name&quot;, at least in portuguese</translatorcomment>
+        <translation>Pesquisar ou escrever URL</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="31"/>
+        <source>Settings</source>
+        <translation>Opções</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="61"/>
+        <source>General</source>
+        <translation>Geral</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="79"/>
+        <source>Start page</source>
+        <translation>Página inicial</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="118"/>
+        <source>Appearance</source>
+        <translation>Aparência</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="134"/>
+        <source>Enable dashboard</source>
+        <translation>Activar painel</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="155"/>
+        <source>Integrated addressbars</source>
+        <translation>Barra de endereço integrada</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="175"/>
+        <source>Colorize the entire tab and toolbar</source>
+        <translation>Colorizar as barras de marcadores e ferramentas inteiramente</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="190"/>
+        <source>Current Browser Version: 0.3</source>
+        <translation>Versão Actual do Browser: 0.3</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="204"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="211"/>
+        <source>Primary Color</source>
+        <translation>Cor Primária</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="232"/>
+        <source>Accent Color</source>
+        <translation>Cor de Acentuação</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="306"/>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Settings.qml" line="322"/>
+        <source>Reset</source>
+        <translatorcomment>There is no translation for &quot;reset&quot; in portuguese (pt_PT)</translatorcomment>
+        <translation>Reset</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDrawer</name>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="30"/>
+        <source>Settings</source>
+        <translation>Opções</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="41"/>
+        <source>General</source>
+        <translation>Geral</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="53"/>
+        <source>Start page</source>
+        <translation>Página inicial</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="87"/>
+        <source>Appearance</source>
+        <translation>Aparência</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="102"/>
+        <source>Enable dashboard</source>
+        <translation>Activar painel</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="122"/>
+        <source>Integrated addressbars</source>
+        <translation>Barra de endereço integrada</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="142"/>
+        <source>Colorize the entire tab and toolbar</source>
+        <translation>Colorizar as barras de marcadores e ferramentas inteiramente</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="157"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="164"/>
+        <source>Primary Color</source>
+        <translation>Cor Primária</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="186"/>
+        <source>Accent Color</source>
+        <translation>Cor de Acentuação</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="230"/>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDrawer.qml" line="245"/>
+        <source>Abort</source>
+        <translation>Abortar</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="5"/>
+        <source>Settings</source>
+        <translation>Opções</translation>
+    </message>
+</context>
+<context>
+    <name>TabsList</name>
+    <message>
+        <location filename="../src/qml/TabsList.qml" line="141"/>
+        <source>Add tab</source>
+        <translation>Adicionar separador</translation>
+    </message>
+</context>
+<context>
+    <name>TabsListPage</name>
+    <message>
+        <location filename="../src/qml/TabsListPage.qml" line="7"/>
+        <source>Tabs</source>
+        <translation>Separadores</translation>
+    </message>
+</context>
+<context>
+    <name>UbuntuOmniboxOverlay</name>
+    <message>
+        <location filename="../src/qml/UbuntuOmniboxOverlay.qml" line="37"/>
+        <source>Search or enter website name</source>
+        <translatorcomment>URL would be a better term for &quot;website name&quot;, at least in portuguese</translatorcomment>
+        <translation>Pesquisar ou escrever URL</translation>
+    </message>
+</context>
+</TS>
